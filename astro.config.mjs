2098c918f94c00f1c2e8a826f963f198920ecbec
ha.nguyenthanh <ha.nguyenthanh@asnet.com.vn>
2026-01-14 09:04:58 +0700

    feat: update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://skincare-commerce.pages.dev",
+  site: 'https://skincare-shop.pages.dev',
 
   integrations: [react({ experimentalReactChildren: true }), sitemap()],
 
