a8d78f24c2a94494bcbe8b2f72137efb5ceff24e
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2026-01-15 10:36:03 +0700

    chore: setup astro cloudflare pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,36 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import cloudflare from '@astrojs/cloudflare';
-import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+
+import cloudflare from '@astrojs/cloudflare';
+import node from '@astrojs/node';
+
+const isCloudflare = import.meta.env.CF_PAGES === '1';
 
 export default defineConfig({
-  site: 'https://skincare-shop.pages.dev',
+  site: isCloudflare ? 'https://skincare-shop.pages.dev' : 'http://localhost:4321',
 
   output: 'server',
 
-  adapter: cloudflare(),
+  adapter: isCloudflare ? cloudflare() : node({ mode: 'standalone' }),
 
   integrations: [react({ experimentalDisableStreaming: true }), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
+    ...(isCloudflare && {
+      ssr: {
+        target: 'webworker',
+        noExternal: ['react', 'react-dom'],
+      },
+      resolve: {
+        alias: {
+          'react-dom/server': 'react-dom/server.edge',
+          'react-dom/server.browser': 'react-dom/server.edge',
+        },
+      },
+    }),
   },
 
   i18n: {
